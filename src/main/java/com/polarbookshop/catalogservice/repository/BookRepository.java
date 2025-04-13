cdfe6d095eb31fb175783d95f8d7759bb3eed87d
sonny <sonny.7n@gmail.com>
2025-04-12 20:47:42 -0500

    five commit

--- src/main/java/com/polarbookshop/catalogservice/repository/BookRepository.java
+++ src/main/java/com/polarbookshop/catalogservice/repository/BookRepository.java
@@ -1,13 +1,23 @@
 package com.polarbookshop.catalogservice.repository;
 
 import com.polarbookshop.catalogservice.domain.Book;
+import org.springframework.data.jdbc.repository.query.Modifying;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-public interface BookRepository {
-    Iterable<Book> findAll();
+@Repository
+public interface BookRepository
+        extends CrudRepository<Book,Long> {
+
     Optional<Book> findByIsbn(String isbn);
     boolean existsByIsbn(String isbn);
-    Book save(Book book);
+    @Modifying
+    @Transactional
+    @Query("delete from Book where isbn = :isbn")
     void deleteByIsbn(String isbn);
+
 }
