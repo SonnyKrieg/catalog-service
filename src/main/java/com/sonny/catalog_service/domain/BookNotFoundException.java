2e36652b8bddf95e012a3a8a0dd391821fb02322
sonny <sonny.7n@gmail.com>
2025-10-07 17:38:33 -0500

    Primer commit

---
+++ src/main/java/com/sonny/catalog_service/domain/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package com.sonny.catalog_service.domain;
+
+public class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(String isbn) {
+        super("The book with ISBN " + isbn + " was not found.");
+    }
+}
+
+
